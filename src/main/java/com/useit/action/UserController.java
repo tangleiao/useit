151805f0a0ec24ef712153fc9b10f518fdc2f174
tangleitao <tangleitao@xin.com>
2018-03-14 11:53:59 +0800

    test

--- src/main/java/com/useit/action/UserController.java
+++ src/main/java/com/useit/action/UserController.java
@@ -1,6 +1,7 @@
 package com.useit.action;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -9,28 +10,36 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class UserController {
 
+
+	
 	@RequestMapping(value = "/user-list", method = RequestMethod.GET)
 	public String userList(ModelMap model) {
 		System.out.println("This is user's page");
 		model.addAttribute("message", "Hello Spring MVC Framework!");
-		return "user/user-list";
+		return "user/list";
 	}
 	
 	@RequestMapping(value = "/add-user", method = RequestMethod.GET)
 	public String addUser() {
-		return "user/add-user";
+		return "user/add";
 	}
 	
 	@RequestMapping(value="/edit-user", method=RequestMethod.GET)
 	public String editUser(ModelMap model) {
 		
 		model.addAttribute("message", "Edit-user");
-		return "edit-user";
+		return "user/edit";
 	}
 	
 	@RequestMapping(value="/delete", method=RequestMethod.POST)
 	public boolean deleteUser(ModelMap map) {
 		return true;
+	}
+	
+	@RequestMapping(value="/hello", method=RequestMethod.GET)
+	public String hello(ModelMap model) {
+		model.addAttribute("message", "Hello Spring MVC Framework!");
+		return "hello";
 	}
 	
 }
